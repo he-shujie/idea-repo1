9fb693e9d5a90dafa1da2884e55ac6fd3b671635
heshujie <1787600256@qq.com>
2022-07-05 00:07:42 +0800

    再一次修改添加

---
+++ TestGitRespository/src/future/fjp/git/HelloJava.java
@@ -0,0 +1,15 @@
+package future.fjp.git;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("JAVA");
+        System.out.println("Hi");
+        System.out.println("java");
+    }
+
+    public void test(){
+        int a = 10;
+        int b = 9;
+    }
+}
