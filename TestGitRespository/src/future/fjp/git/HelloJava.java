b5247715e50a699c4c4d5b471cff7dcb68781298
heshujie <1787600256@qq.com>
2022-07-05 00:24:34 +0800

    本地克隆修改

--- TestGitRespository/src/future/fjp/git/HelloJava.java
+++ TestGitRespository/src/future/fjp/git/HelloJava.java
@@ -6,10 +6,13 @@ public class HelloJava {
         System.out.println("JAVA");
         System.out.println("Hi");
         System.out.println("java");
+        System.out.println("IDEA集成GIT");
     }
 
     public void test(){
         int a = 99;
         int b = 100;
+        int ab =a+b;
+        System.out.println("计算结果："+ab);
     }
 }
